4578da18b84017d390ca1039eb9853006639912b
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-16 17:57:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,11 +3,5 @@ def badge_maker(name)
 end 
 
 def batch_badge_creator(attendees)
-  count = 0 
-  new_array = []
-  while count < attendees.length do 
-    new_array << "Hello, my name is #{attendees}"
-    count += 1
-  end 
-  new_array
-end 
+ "Hello, my name is #{attendees}"
+ end 
