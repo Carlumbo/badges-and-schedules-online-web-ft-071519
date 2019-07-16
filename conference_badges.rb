6f93590e12f5ee5ba05c7e7da5afd6b44c5d0ca6
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-16 17:48:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,18 +1,3 @@
 def badge_maker(name)
-  "Hello, my name is #{name}."
-end 
-
-
-def batch_badge_creator(attendees)
-new_array = []
-  attendees.each { |badges| new_array << "Hello, my name is #{badges}."}
-  new_array
-end 
-
-def assign_rooms(attendees)
-  new_array = [] 
-  # room_assigment = attendees.each_with_index + 1 
-  attendees.each { |room| 
-  room_assigment = attendees.count + 1  new_array << "blank #{room} badges #{room_assignment}" }
-  new_array
+  puts "Hello, My name is #{name}"
 end 
